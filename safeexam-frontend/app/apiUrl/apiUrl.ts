128f6655af92b50fbaa9b6db94b875d2af0167d9
drashtipatelGAXL <drashti.patel@growthaxl.com>
2025-02-27 17:18:26 +0530

    Added API to download EXE and Json File.

--- safeexam-frontend/app/apiUrl/apiUrl.ts
+++ safeexam-frontend/app/apiUrl/apiUrl.ts
@@ -1,5 +1,5 @@
-// const baseUrl = "https://b734-2405-201-200a-f0a7-315e-244e-b529-1ffb.ngrok-free.app/";
-const baseUrl = "http://127.0.0.1:3333/"
+const baseUrl = "https://b734-2405-201-200a-f0a7-315e-244e-b529-1ffb.ngrok-free.app/";
+
 interface ApiEndpoints {
   LoginUrl: string;
   ExamMetadata: string;
